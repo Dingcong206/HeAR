18b49901f858dc0c9f567419acef465f10b53531
Dingcong206 <dingcong882@gmail.com>
2026-01-26 11:25:26 +0900

    h

--- binarybimamba.py
+++ binarybimamba.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader, TensorDataset
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import roc_auc_score, confusion_matrix, classification_report
+from sklearn.metrics import roc_auc_score, confusion_matrix, f1_score, classification_report
 
 try:
     from mamba_ssm import Mamba
